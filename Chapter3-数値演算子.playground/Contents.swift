2a096353bb0f9ff8294667c737c27c4cf1979d53
MasaharuMisaki <masaharu7532@icloud.com>
2023-08-27 14:52:03 +0900

    Initial Commit

---
+++ Chapter3-数値演算子.playground/Contents.swift
@@ -0,0 +1,11 @@
+// 3.1 53から3を引いた結果を整数でprintしてください。
+var number = 53 - 3
+print(number)
+
+// 3.2 4を2で割った結果をprintしてください。
+var wari = 4 / 2
+print(wari)
+
+// 3.3 4と2.5をかけた結果をprintしてください。
+var kake = 3.3 * 2.5
+print(kake)
